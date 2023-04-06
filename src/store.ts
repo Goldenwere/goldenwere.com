5401d73da328f0b8dcdc4119fcd70e983cc2a3fd
Lightling <contactlightling@gmail.com>
2023-04-06 17:49:01 -0400

    begin setting up home

--- src/store.ts
+++ src/store.ts
@@ -3,6 +3,7 @@ import type { Site } from '@/src/types/site'
 
 export const useStore = defineStore('store', {
   state: () => ({
-    site: {} as Site
+    site: {} as Site,
+    home: undefined as unknown as Object,
   }),
 })
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,10 +1,31 @@
 <script setup lang='ts'>
+import { reactive } from 'vue'
+import { useRoute } from 'vue-router'
+import { fetchAndParseContent } from '@/src/utilities/fetch'
+import { useStore } from '@/src/store'
+import GwHeader from '@/src/components/navigation/header/GwHeader.vue'
+import GwFooter from '@/src/components/navigation/footer/GwFooter.vue'
 
+const store = useStore()
+const route = useRoute()
+const content = reactive(store.home)
+
+if (store.home === undefined) {
+  fetchAndParseContent('/content/home.yml')
+    .then((content) => {
+      store.$patch({ home: content as any})
+      console.log(store.home)
+    })
+}
 </script>
 
 <template lang='pug'>
-.soon
-  p Soon...
+.content
+  .about
+    p(
+      v-for='p in content.about'
+      v-html='p'
+    )
 </template>
 
 <style scoped lang='sass'>

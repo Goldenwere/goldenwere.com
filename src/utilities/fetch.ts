a13aa089acc1d2b1202726d6e7117a473e3c5e94
Lightling <contact@lightling.xyz>
2023-04-12 17:29:49 -0400

    add sanitization

--- src/utilities/fetch.ts
+++ src/utilities/fetch.ts
@@ -1,8 +1,68 @@
 import yaml from 'js-yaml'
+import DOMPurify from 'dompurify'
+
+const domPurifyConfig = {
+  ALLOW_ARIA_ATTR: true,
+  ALLOWED_ATTR: [
+    'alt',
+    'href',
+    'style',
+    'title',
+  ],
+  ALLOWED_TAGS: [
+    'a',
+    'p',
+    'span',
+
+    'b',
+    'em',
+    'i',
+    'mark',
+    's',
+    'sub',
+    'sup',
+    'small',
+    'strike',
+    'strong',
+    'u',
+
+    'abbr',
+    'audio',
+    'blockquote',
+    'cite',
+    'code',
+    'data',
+    'dfn',
+    'div',
+    'img',
+    'pre',
+    'samp',
+    'time',
+    'track',
+    'var',
+    'video',
+
+    'figcaption',
+    'figure',
+
+    'br',
+    'hr',
+    'wbr',
+
+    'ol',
+    'ul',
+    'li',
+
+    'dd',
+    'dl',
+    'dt',
+  ],
+}
 
 export const fetchAndParseContent = async (path: string) => {
   const response = await fetch(path)
   const blob = await response.blob()
   const yamlAsString = await blob.text()
-  return yaml.load(yamlAsString)
+  const sanitized = DOMPurify.sanitize(yamlAsString, domPurifyConfig)
+  return yaml.load(sanitized)
 }
